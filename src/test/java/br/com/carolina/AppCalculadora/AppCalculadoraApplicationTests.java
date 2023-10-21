15128344d2f33c0ab44b94e64c3816748ff40128
Carolina Veloso <annacarolinacampos@hotmail.com>
2023-10-21 10:49:31 -0300

    inicialização do projeto

---
+++ src/test/java/br/com/carolina/AppCalculadora/AppCalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carolina.AppCalculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
